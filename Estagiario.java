8690aa918d0a536a1810bee057b66074804139ec
Vinicius Sampaio <vibasampaio@hotmail.com>
2021-05-16 21:54:22 +0000

    herança

--- Estagiario.java
+++ Estagiario.java
@@ -1,7 +1,4 @@
 public class Estagiario extends Funcionario{
-  private int ID;
-  private String nome;
-  private double salario;
   private double vale_coxinha;
   public Estagiario(int ID, String nome, double salario, double vale_coxinha){
     super(ID, nome, salario);
@@ -9,9 +6,5 @@ public class Estagiario extends Funcionario{
   }
   public void setValeCoxinha( double vale_coxinha ){
     this.vale_coxinha = vale_coxinha;
-  }
-  public double getSalario(){
-  // Calcula o salário líquido
-    return(this.salario+this.vale_coxinha);
   }
 }
--- Main.java
+++ Main.java
@@ -5,10 +5,13 @@ class Main {
   public static void main(String[] args) {
     Funcionario funcionario;
     Empresa vns;
+    int ID = 0;
     vns = new Empresa("VNS LTDA");
     Utils utils = new Utils();
     Scanner opcaoInicio = new Scanner(System.in);
     Scanner opcaoCargo = new Scanner(System.in);
+    Scanner nomeScan = new Scanner(System.in);
+    Scanner salarioScan = new Scanner(System.in);
     System.out.println("Bem vindo ao sistema de gerenciamento de funcionários da empresa XYZ");
     System.out.println("Escolha uma das opções a seguir:");
     String[] opcoes = new String[]{"Adicionar um funcionário", "Calcular pagamento do funcionário", "Aumentar o adicional de todos os funcionários", "Relatório dos funcionários da empresa"};
@@ -18,12 +21,36 @@ class Main {
     switch (opcaoMetodo)
     {
         case "1":
-        //Java code
-        UUID uuid=UUID.randomUUID();
-        mostrarOpcoes(cargos);
-        String opcao = opcaoCargo.nextInt();
-        funcionario = new Funcionario(uuid, "Vinicius", 1250.0, cargos[opcao]);
+          //Java code
+          ID++;
+          utils.mostrarOpcoes(cargos);
+          int opcao = opcaoCargo.nextInt();
+          System.out.println(cargos[opcao]);
+          switch(cargos[opcao])
+          {
+            case "Estagiário":
+              Estagiario estag;
+              System.out.println("Nome Estagiário: ");
+              String nome = nomeScan.nextLine();
+              System.out.println("Salário Estagiário: "):
+              double salario = salarioScan.nextDouble();
+              estag = new Estagiario(ID, nome, salario);
+            
+            // 
+            ;
+            case "Gerente":
+            // 
+            ;
+            case "Presidente":
+            // 
+            ;
+            case "Secretária":
+            // 
+            ;
+          }
+        funcionario = new Funcionario(ID, "Vinicius", 1250.0);
         vns.adicionarFuncionario(funcionario);
+        
         ;
         case "2":
         //Java code
